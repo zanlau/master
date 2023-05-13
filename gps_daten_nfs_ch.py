86d3492ad53ac23c803c767485bfa7a17a89e8f5
zanlau <laura.zanchin@bluewin.ch>
2023-05-13 14:32:47 +0200

    gps daten nfs schweiz

--- gps_daten_nfs_ch.py
+++ gps_daten_nfs_ch.py
@@ -4,22 +4,23 @@ import urllib.parse
 from openpyxl import load_workbook
 
 
-wb = load_workbook('daten/notfallstationen_ch_copy.xlsx')
-worksheet = wb['Standorte 2021 KZP21']
+wb = load_workbook('daten/notfallstationen_ch.xlsx')
+worksheet = wb['KZ2021_KZP21']
 
 stations = []
 
 for row in worksheet.iter_rows(min_row=2): #von erster Spalte über alle Elemente iterieren
-    address = f"{row[8].value}, {row[9].value}"
+    address = f"{row[3].value}, {row[4].value}, {row[5].value}"
     print(address)
     url = f'https://nominatim.openstreetmap.org/search/{urllib.parse.quote(address)}?format=json'
     response = requests.get(url).json()
     stations.append({
         "lat": response[0]["lat"],
         "lon": response[0]["lon"],
-        "institut": row[7].value,
-        "stand_address": row[8].value,
-        "stand_ort": row[9].value
+        "institut": row[2].value,
+        "stand_address": row[3].value,
+        "stand_plz": row[4].value,
+        "stand_ort": row[5].value
     })
 
 data = {
