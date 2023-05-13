b23b9ea336d577ac975071711ac3ce4b2bff60a9
zanlau <laura.zanchin@bluewin.ch>
2023-05-13 13:36:54 +0200

    gpd daten nfs lu

--- gps_daten_nfs_lu.py
+++ gps_daten_nfs_lu.py
@@ -4,26 +4,29 @@ import urllib.parse
 from openpyxl import load_workbook
 
 
-wb = load_workbook('')
-worksheet = wb['']
+wb = load_workbook('daten/notfallstationen_lu.xlsx')
+worksheet = wb['Tabelle1']
 
 stations = []
 
-for row in worksheet.iter_rows(min_row=?): #von erster Spalte über alle Elemente iterieren
-    address = ?
+for row in worksheet.iter_rows(min_row=2): #von erster Spalte über alle Elemente iterieren
+    address = f"{row[1].value}, {row[2].value}, {row[3].value}"
     print(address)
     url = f'https://nominatim.openstreetmap.org/search/{urllib.parse.quote(address)}?format=json'
     response = requests.get(url).json()
     stations.append({
         "lat": response[0]["lat"],
         "lon": response[0]["lon"],
-        "institut": ?,
-        "stand_address": ?,
-        "stand_ort": ?
+        "institut": row[0].value,
+        "stand_address": row[1].value,
+        "stand_plz": row[2].value,
+        "stand_ort": row[3].value,
     })
 
 data = {
-    "source": "?",
+    "source": "Händische Erfassung mit Beizug der Literatur "
+              "https://mediateursante.public.lu/de/soins-luxembourg/prestlux/hopitaux.html und"
+              "https://www.editus.lu/de/karte?q=Notaufnahme&filters=5:7315;",
     "data": stations
 }
 
