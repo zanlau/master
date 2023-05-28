6138afd580da7ced658495480e1e48ec031af6f2
zanlau <laura.zanchin@bluewin.ch>
2023-05-28 15:43:55 +0200

    Dataframes für Notfallarten

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from dash import dcc
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import dcc
-from dash import html
+from dash import dcc, html, ctx
 from dash.dependencies import Input, Output
 from flask import Flask
 from openpyxl import load_workbook
@@ -22,13 +21,13 @@ def h4(text): return html.H4(text, style={'text-align': 'left'})
 
 def graph(id): return dcc.Graph(id=id, figure={})
 
-#Interaction possibilities
+#Interaktionsmöglichkeiten
 def emergency_dropdown(id):
     return dcc.Dropdown(id=id,
                         options=[
                             {"label": "Medizinische Notfallstation", "value": 'nfs'},
-                            {"label": "Feuerwehrstützpunk", "value": 'fire_station'},
-                            {"label": "Schlaganfallzentren", "value": 'stroke'}],
+                            {"label": "Feuerwehrstützpunkt", "value": 'fire_station'},
+                            {"label": "Schlaganfallzentrum", "value": 'stroke'}],
                         multi=False,
                         value='emergency',
                         style={"width": "40%"})
@@ -37,10 +36,10 @@ def area_criteria(id):
     return dcc.Dropdown(id=id,
                         options=[
                             {"label": "15 Minuten", "value": 'minutes'},
-                            {"label": "Gebietsfläche", "value": 'kilometer'},
+                            {"label": "Gebietsfläche", "value": 'area'},
                             {"label": "Bevölkerungsgrösse", "value": 'population'}],
                         multi=False,
-                        value='minutes',
+                        value='criteria',
                         style={"width": "40%"})
 
 def row(children):
@@ -61,7 +60,7 @@ app.layout = html.Div(
                     dbc.CardHeader(
                         "Notfallart"),
                     dbc.CardBody([
-                        emergency_dropdown('emergency_art')
+                        emergency_dropdown('emergency_type')
                     ])
                 ])
             ])
@@ -72,7 +71,7 @@ app.layout = html.Div(
                     dbc.CardHeader(
                         "Kriterien der Einzugsgebiete"),
                     dbc.CardBody([
-                        area_criteria('einzugsgebiete')
+                        area_criteria('criteria')
                     ])
                 ])
             ])
@@ -90,9 +89,9 @@ app.layout = html.Div(
     ]
 )
 
-#create graphs in dashboard
-#map in dash to deploy stations (OSM)
-def get_fig():
+#Diagramme im Dashboard generieren
+#Positionen in OpenStreetMap erzeugen
+def get_fig(emergency_type):
     with open("daten/notfallstationen_ch.json") as f1:
         data_1 = json.load(f1)
     with open("daten/notfallstationen_lu.json") as f2:
@@ -103,8 +102,10 @@ def get_fig():
         data_4 = json.load(f4)
     with open("daten/fire_station_ch.json") as f5:
         data_5 = json.load(f5)
+        data_5["data"] = data_5.pop("elements")
     with open("daten/fire_station_lu.json") as f6:
         data_6 = json.load(f6)
+        data_6["data"] = data_6.pop("elements")
 
     colors = ["blue", "green", "red"]
 
@@ -114,17 +115,17 @@ def get_fig():
             go.Scattermapbox(
                 lat=[x["lat"] for x in data_1["data"]] + [x["lat"] for x in data_2["data"]]
                     + [x["lat"] for x in data_3["data"]] + [x["lat"] for x in data_4["data"]]
-                    + [x.get("lat", None) for x in data_5["elements"]] + [x.get("lat", None) for x in data_6["elements"]],
+                    + [x.get("lat", None) for x in data_5["data"]] + [x.get("lat", None) for x in data_6["data"]],
                 lon=[x["lon"] for x in data_1["data"]] + [x["lon"] for x in data_2["data"]]
                     + [x["lon"] for x in data_3["data"]] + [x["lon"] for x in data_4["data"]]
-                    + [x.get("lon", None) for x in data_5["elements"]] + [x.get("lon", None) for x in data_6["elements"]],
+                    + [x.get("lon", None) for x in data_5["data"]] + [x.get("lon", None) for x in data_6["data"]],
                 mode="markers",
                 marker=dict(size=15, color=[colors[0]] * len(data_1["data"])
                                                               + [colors[0]] * len(data_2["data"])
                                                               + [colors[1]] * len(data_3["data"])
                                                               + [colors[1]] * len(data_4["data"])
-                                                              + [colors[2]] * len(data_5["elements"])
-                                                              + [colors[2]] * len(data_6["elements"])),
+                                                              + [colors[2]] * len(data_5["data"])
+                                                              + [colors[2]] * len(data_6["data"])),
             )
         )
 
@@ -138,15 +139,43 @@ def get_fig():
         )
     return fig
 
+
+
+def filter_data_based_on_criteria(emergency_type):
+    # Filter basierend auf Notfallart + Laden der Daten
+    if emergency_type == 'Medizinische Notfallstation':
+        df_1 = pd.read_json("daten/notfallstationen_ch.json")
+        df_2 = pd.read_json("daten/notfallstationen_lu.json")
+        return pd.concat([df_1, df_2])
+    elif emergency_type == 'Schlaganfallzentrum':
+        df_3 = pd.read_json("daten/stroke_units_ch.json")
+        df_4 = pd.read_json("daten/stroke_untis_lu.json")
+        return pd.concat([df_3, df_4])
+    elif emergency_type == 'Feuerwehrstützpunkt':
+        df_5 = pd.read_json("daten/fire_station_ch.json")
+        df_6 = pd.read_json("daten/fire_station_lu.json")
+        return pd.concat([df_5, df_6])
+    raise "Keine gültige Notfallart" #wenn keine der obigen Arten
+
+
 @app.callback(
     Output("graph", "figure"),
-    Input("my_interval", "n_intervals")
+    inputs={
+        "all_inputs": {
+            "emergency_type": Input("emergency_type", "value"),
+            "my_interval": Input("criteria", "value")
+        }
+    },
 )
-
-
-
-def update_bar_chart(dummy):
-    return get_fig()
+def update_bar_chart(all_inputs):
+    #data = filter_data_based_on_criteria(value)
+    c = ctx.args_grouping.all_inputs
+    if c.emergency_type.triggered:
+        print(f"{c.emergency_type.value}")
+    elif c.my_interval.triggered:
+        print(f"{c.my_interval.value}")
+    fig = get_fig(c.emergency_type.value)
+    return fig
 
 if __name__ == '__main__':
     app.run_server()
