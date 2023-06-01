b3996fb001a5d38c37941350a45caf4acbc2e1f6
zanlau <laura.zanchin@bluewin.ch>
2023-06-01 16:07:05 +0200

    Zweite Seite für Vergleich

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ import plotly.express as px
 import plotly.graph_objects as go
 from dash import dcc, html, ctx
 from dash.dependencies import Input, Output
+import dash.dependencies as dependencies
 from flask import Flask
 from openpyxl import load_workbook
 from plotly.subplots import make_subplots
 
 
-
 def h1(text): return html.H1(text, style={'text-align': 'center'})
+
 
 def h4(text): return html.H4(text, style={'text-align': 'left'})
 
-#Interaktionsmöglichkeiten
+
+# Interaktionsmöglichkeiten
 def emergency_dropdown(id):
     return dcc.Dropdown(id=id,
                         options=[
@@ -29,6 +31,7 @@ def emergency_dropdown(id):
                         multi=False,
                         value='emergency_type',
                         style={"width": "40%"})
+
 
 def area_criteria(id):
     return dcc.Dropdown(id=id,
@@ -40,6 +43,7 @@ def area_criteria(id):
                         value='criteria',
                         style={"width": "40%"})
 
+
 def row(children):
     return dbc.Row(children, style={"margin-bottom": "2rem"})
 
@@ -48,57 +52,97 @@ server = Flask(__name__)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 app.title = "Dashboard Notfallversorgungen"
 
-app.layout = html.Div(
-    [
-        h1("Einzugsgebiete von Notfallversorgungen"),
+app.layout = html.Div([
+    html.Div(style={'height': '10px'}), #Leerzeile einfüge
+    h1("Einzugsgebiete von Notfallversorgungen"),
+    html.Div(style={'height': '10px'}), #Leerzeile einfügen
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Karte mit Einzugsgebiete', value='tab-1'),
+        dcc.Tab(label='Charts für Vergleiche', value='tab-2'),
+    ]),
+    html.Div(id='page-content')
+])
 
-        row([
-            dbc.Col(width=12, children=[
-                dbc.Card([
-                    dbc.CardHeader(
-                        "Notfallart"),
-                    dbc.CardBody([
-                        emergency_dropdown('emergency_type')
+
+@app.callback(
+    dash.dependencies.Output('page-content', 'children'),
+    [dash.dependencies.Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.Div(
+                row([
+                    dbc.Col(width=12, children=[
+                        dbc.Card([
+                            dbc.CardHeader(
+                                "Notfallart"),
+                            dbc.CardBody([
+                                emergency_dropdown('emergency_type')
+                            ])
+                        ])
                     ])
-                ])
-            ])
-        ]),
-        row([
-            dbc.Col(width=12, children=[
-                dbc.Card([
-                    dbc.CardHeader(
-                        "Kriterien der Einzugsgebiete"),
-                    dbc.CardBody([
-                        area_criteria('criteria')
+                ])),
+            html.Div(
+                row([
+                    dbc.Col(width=12, children=[
+                        dbc.Card([
+                            dbc.CardHeader(
+                                "Kriterien der Einzugsgebiete"),
+                            dbc.CardBody([
+                                area_criteria('criteria')
+                            ])
+                        ])
                     ])
-                ])
-            ])
-        ]),
-        row([
-            dbc.Col(width=12, children=[
-                dbc.Card([
-                    dbc.CardHeader(children="Karte zur Bestimmung der Einzugsgebiete",
-                        id="world_map_header"),
-                    dcc.Graph(id="graph"),
-                    dcc.Interval(id="my_interval", interval=2000, n_intervals=0, disabled=False),
-                ])
-            ])
-        ]),
-    ]
-)
+                ])),
+            html.Div(
+                row([
+                    dbc.Col(width=12, children=[
+                        dbc.Card([
+                            dbc.CardHeader(children="Karte zur Bestimmung der Einzugsgebiete",
+                                           id="world_map_header"),
+                            dcc.Graph(id="graph"),
+                            dcc.Interval(id="my_interval", interval=2000, n_intervals=0, disabled=False),
+                        ])
+                    ])
+                ]))
+        ])
+
+    elif tab == 'tab-2':
+        return html.Div([
+            dcc.Graph(
+                id='bar-chart',
+                figure={
+                    'data': [
+                        go.Bar(
+                            x=x_data,
+                            y=y_data,
+                            marker=dict(color='blue')  # Farbe der Balken
+                        )
+                    ],
+                    'layout': go.Layout(
+                        title='Bar Chart Beispiel',  # Titel des Charts
+                        xaxis=dict(title='Kategorien'),  # Beschriftung der x-Achse
+                        yaxis=dict(title='Werte')  # Beschriftung der y-Achse
+                    )
+                }
+            )
+        ])
+
 
 class Cord:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
 
+
 class NFS:
     def __init__(self, kind, country, coordinates):
         self.kind = kind
         self.country = country
         self.coordinates = coordinates
-        #self.personal = personal
+        # self.personal = personal
         self.color = {"notfallstation": "blue", "stroke_unit": "green", "fire_station": "red"}[self.kind]
+
 
 def open_data():
     data = []
@@ -117,28 +161,29 @@ def open_data():
     return data
 
 
-#Diagramme im Dashboard generieren
-#Positionen in OpenStreetMap erzeugen
+# Diagramme im Dashboard generieren
+# Positionen in OpenStreetMap erzeugen
 def get_fig(data):
     fig = go.Figure()
 
     fig.add_trace(
-            go.Scattermapbox(
-                lat=[x.coordinates.lat for x in data],
-                lon=[x.coordinates.lon for x in data],
-                mode="markers",
-                marker=dict(size=15, color=[x.color for x in data]),
-            )
+        go.Scattermapbox(
+            lat=[x.coordinates.lat for x in data],
+            lon=[x.coordinates.lon for x in data],
+            mode="markers",
+            marker=dict(size=15, color=[x.color for x in data]),
         )
+    )
 
     # open street map mit Standardposition
     fig.update_layout(
-            height=1500,
-            width=2500,
-            margin={"r": 10, "t": 10, "b": 10, "l": 10},
-            autosize=False,
-            mapbox=dict(style="open-street-map", center=dict(lat=47.95, lon=7.45), zoom=7, uirevision="true"), #Deaktivierung der automatischen Rückkehr zur Standardposition
-        )
+        height=1500,
+        width=2500,
+        margin={"r": 10, "t": 10, "b": 10, "l": 10},
+        autosize=True,
+        mapbox=dict(style="open-street-map", center=dict(lat=47.95, lon=7.45), zoom=7, uirevision=len(data)),
+        # Deaktivierung der automatischen Rückkehr zur Standardposition
+    )
     return fig
 
 
@@ -160,7 +205,7 @@ def filter_data_based_on_criteria(data, kind):
 )
 def update_map(all_inputs):
     data = open_data()
-    c = ctx.args_grouping.all_inputs
+    c = ctx.args_grouping["all_inputs"]
     if c.emergency_type.triggered:
         print(f"{c.emergency_type.value}")
         data = filter_data_based_on_criteria(data, c.emergency_type.value)
@@ -169,6 +214,8 @@ def update_map(all_inputs):
 
     return get_fig(data)
 
+x_data = ['A', 'B', 'C', 'D']
+y_data = [10, 8, 12, 6]
 
 if __name__ == '__main__':
     app.run_server()
