13a2b3c55f448c65e1f2d57aa660bc59c60503cd
zanlau <laura.zanchin@bluewin.ch>
2023-06-11 01:55:18 +0200

    OSM ID Verlinkung
    URL-Suche nur Schweiz
    Weitere types hinzugefügt
    Sicherstellung dass alle Gemeinden abgearbeitet werden

--- population_ch_data_gemeinde.py
+++ population_ch_data_gemeinde.py
@@ -10,28 +10,35 @@ worksheet = wb['Gemeinde']
 municipality = []
 
 for row in worksheet.iter_rows(min_row=2): #von erster Spalte über alle Elemente iterieren
-    numbers = f"{row[1].value}"
+    numbers = f"{row[1].value}".replace("/", " ")
     print(numbers)
-    url = f'https://nominatim.openstreetmap.org/search/{urllib.parse.quote(numbers)}?format=json'
+    url = f'https://nominatim.openstreetmap.org/search/{urllib.parse.quote(numbers)}?format=json&countrycodes=ch'
     response = requests.get(url).json()
     print(response)
 
     for i, item in enumerate(response):
-        url2 = f'https://nominatim.openstreetmap.org/lookup?osm_ids=R{item["osm_id"]}&format=json'
-        response2 = requests.get(url2).json()[0]
-        print(response2)
-        address = response2["address"]
-        if ("city" in address or "town" in address or "village" in address) and address["country_code"] == "ch":
-            municipality.append({
-                "lat": response[i]["lat"],
-                "lon": response[i]["lon"],
-                "number": row[0].value,
-                "name": row[1].value,
-                "population": row[2].value,
-                "osm_id": response[i]["osm_id"]
-            })
-            break
+        osm_type = {"relation": "R", "node": "N", "way": "W"}[item["osm_type"]] #alle osm_types integrieren, da diese sonst nicht abgerufen werden können
+
+        if osm_type == "R" or (osm_type == "N" and item["class"] == "place" and item["type"] == "village"):
+            url2 = f'https://nominatim.openstreetmap.org/lookup?osm_ids={osm_type}{item["osm_id"]}&format=json'
+            response2 = requests.get(url2).json()
+            print(response2)
+            address = response2[0]["address"]
+            matchers = ["city", "town", "village", "municipality", "hamlet", "suburb"]
+            if any(x in address for x in matchers) and address["country_code"] == "ch":
+                municipality.append({
+                    "lat": response[i]["lat"],
+                    "lon": response[i]["lon"],
+                    "number": row[0].value,
+                    "name": row[1].value,
+                    "population": row[2].value,
+                    "osm_id": response[i]["osm_id"]
+                })
+                break
         print("NEXT TRY")
+    else:
+        print("AAAAAAAAAA")
+        raise "AAAA"
 
 
 data = {
