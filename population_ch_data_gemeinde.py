0fe7da83bf70f875412defbb3dce4b0666960ad7
zanlau <laura.zanchin@bluewin.ch>
2023-06-13 20:09:21 +0200

    cache der response
    Alle Daten auf einmal lokal speichern

--- population_ch_data_gemeinde.py
+++ population_ch_data_gemeinde.py
@@ -1,7 +1,13 @@
 import json
+import base64
 import requests
 import urllib.parse
 from openpyxl import load_workbook
+from pathlib import Path
+
+
+data_directory = "daten/population_ch_gemeinde"
+Path(data_directory).mkdir(parents=True, exist_ok=True)
 
 
 wb = load_workbook('daten/population_ch.xlsx')
@@ -9,20 +15,43 @@ worksheet = wb['Gemeinde']
 
 municipality = []
 
+def get_search_response(place):
+    # use base64 encode to ensure the filename is valid
+    filename = f"{data_directory}/{base64.b64encode(place).rstrip()}.json"
+    try:
+        with open(filename, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        url = f'https://nominatim.openstreetmap.org/search/{urllib.parse.quote(place)}?format=json&countrycodes=ch'
+        response = requests.get(url).json()
+        with open(filename, "w") as f:
+            json.dump(response, f)
+        return response
+
+def get_lookup_response(osm_id):
+    filename = f"{data_directory}/{osm_id}.json"
+    try:
+        with open(filename, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        url = f'https://nominatim.openstreetmap.org/lookup?osm_ids={osm_id}&format=json'
+        response = requests.get(url).json()
+        with open(filename, "w") as f:
+            json.dump(response, f)
+        return response
+
+
 for row in worksheet.iter_rows(min_row=2): #von erster Spalte über alle Elemente iterieren
-    numbers = f"{row[1].value}".replace("/", " ")
-    print(numbers)
-    url = f'https://nominatim.openstreetmap.org/search/{urllib.parse.quote(numbers)}?format=json&countrycodes=ch'
-    response = requests.get(url).json()
-    print(response)
+    place = f"{row[1].value}".replace("/", " ")
+    print(place)
+
+    response = get_search_response(place)
 
     for i, item in enumerate(response):
         osm_type = {"relation": "R", "node": "N", "way": "W"}[item["osm_type"]] #alle osm_types integrieren, da diese sonst nicht abgerufen werden können
 
         if osm_type == "R" or (osm_type == "N" and item["class"] == "place" and item["type"] == "village"):
-            url2 = f'https://nominatim.openstreetmap.org/lookup?osm_ids={osm_type}{item["osm_id"]}&format=json'
-            response2 = requests.get(url2).json()
-            print(response2)
+            response2 = get_lookup_response(f'{osm_type}{item["osm_id"]}')
             address = response2[0]["address"]
             matchers = ["city", "town", "village", "municipality", "hamlet", "suburb"]
             if any(x in address for x in matchers) and address["country_code"] == "ch":
